ec9e5d8386497b0503518ef389d6d9de90b2608a
domchang1 <dominicchang08@gmail.com>
2022-06-29 12:35:13 -0700

    initial commit

---
+++ RNN.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self, inp_dim, out_dim, hidden_dim, n_layers=2):
+        super(RNN, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.inp_dim = inp_dim
+        self.out_dim = out_dim
+        
+        self.rnn = nn.RNN(self.inp_dim, self.hidden_dim, self.n_layers, nonlinearity='relu')
+        self.linear = nn.Linear(self.hidden_dim, self.out_dim)
+    
+    def forward():
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from unicodedata import bidirectional
+import torch
+import torch.nn.functional as F
+#venv\Scripts\activate.bat
+
+
+
