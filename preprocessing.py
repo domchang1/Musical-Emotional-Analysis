49ca170d4da481b5dc333615581e3db760747aca
domchang1 <dominicchang08@gmail.com>
2022-07-04 13:06:22 -0700

    processed labels into emotions

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+
 data = pd.read_csv('song_annotations.txt', delimiter='\t')
 data.columns = ["song", "label"]
 print(data)
 print(data.label.unique())
-# emotions = ["aggressive", "angry", "arousing", "boring", "calming", "cheerful", "cold", 
-# "depressed", "emotional", "exciting", "happy", "lighthearted", "mellow", "morose", "negative", "negative feelings", "normal",
-# "positive", "relax", "romantic", "sad", "strong", ""] #also have not-angry, not-happy, etc.. do we include??
-# data_emotions = pd.loc()
+emotions = ["aggressive", "angry", "arousing", "boring", "calming", "cheerful", "cold", 
+"depressed", "emotional", "exciting", "happy", "lighthearted", "mellow", "morose", "negative", "negative feelings", "normal",
+"not angry - agressive", "not calming", "not happy", "not sad", "pleasant", "positive", "relax", "romantic", "rough", "sad", 
+"strong", "tender", "touching", "unpleasant", "unromantic"] 
+data_emotions = data.loc[data['label'].isin(emotions)]
+print(data_emotions)
