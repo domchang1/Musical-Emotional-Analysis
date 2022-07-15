c3a0a2d0d0a6a1103c83874679f6928e2dab24fc
domchang1 <dominicchang08@gmail.com>
2022-07-14 22:52:00 -0700

    created dictionary of song to emotions

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+import glob
 
 data = pd.read_csv('song_annotations.txt', delimiter='\t')
 data.columns = ["song", "label"]
@@ -10,6 +11,28 @@ emotions = ["aggressive", "angry", "arousing", "boring", "calming", "cheerful", 
 "not angry - agressive", "not calming", "not happy", "not sad", "pleasant", "positive", "relax", "romantic", "rough", "sad", 
 "strong", "tender", "touching", "unpleasant", "unromantic"] 
 data_emotions = data.loc[data['label'].isin(emotions)]
+data_emotions.sort_values(by='song', axis=0, inplace=True)
 print(data_emotions)
-print(len(data_emotions.song.unique()))
-#how to load .mfcc and .mel? should we be doing multivariate classification?
+new_dict = {}
+for index, row in data_emotions.iterrows():
+    try:
+        new_dict[row['song']]
+    except KeyError:
+        new_dict[row['song']] = [row['label']]
+    else:
+        new_dict[row['song']].append(row['label'])
+print(new_dict)
+exit()
+filenames = glob.glob('./mel_features/*.mel')
+mel = {}
+mfcc = {}
+for filename in filenames:
+    song = filename[15:len(filename)-4]
+    mel[song] = np.loadtxt(filename, delimiter=',')
+filenames = glob.glob("./mfcc_features/*.mfcc")
+for filename in filenames:
+    song = filename[16:len(filename)-5]
+    mfcc[song] = np.loadtxt(filename, delimiter=',')
+print(len(mel))
+print(len(mfcc))
+#use sigmoid for multilabel
